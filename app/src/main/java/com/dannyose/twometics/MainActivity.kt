08e724d59ff3fb060b95fb9104b3c5f5d9bff2c1
Danny10ison <pagesngod@gmail.com>
2022-02-14 14:55:21 +0000

    Simple calculator Logic added to UI
    Enter the numbers in the fields and tap on the operation you want.
    Any empty field will be replaced by 0, and a prompt for division by 0

--- app/src/main/java/com/dannyose/twometics/MainActivity.kt
+++ app/src/main/java/com/dannyose/twometics/MainActivity.kt
@@ -8,10 +8,15 @@ import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var firstOperandTextInputLayout: TextInputLayout
+    private lateinit var answerTextView              : TextView
+
+    private lateinit var firstOperandTextInputLayout : TextInputLayout
     private lateinit var secondOperandTextInputLayout: TextInputLayout
-    private lateinit var answerTextView: TextView
-    private lateinit var answerButton: Button
+
+    private lateinit var plusButton                  : Button
+    private lateinit var minusButton                 : Button
+    private lateinit var timesButton                 : Button
+    private lateinit var divideButton                : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,19 +27,112 @@ class MainActivity : AppCompatActivity() {
         firstOperandTextInputLayout = findViewById(R.id.operand_1)
         secondOperandTextInputLayout = findViewById(R.id.operand_2)
         answerTextView   = findViewById(R.id.answer_view)
-        answerButton = findViewById(R.id.calculate_button)
 
 
+        plusButton = findViewById(R.id.add_button)
+        minusButton = findViewById(R.id.minus_button)
+        divideButton = findViewById(R.id.divide_button)
+        timesButton = findViewById(R.id.times_button)
 
-        answerButton.setOnClickListener{
-            val op1:String = firstOperandTextInputLayout.editText?.text.toString()
-            val op2:String = secondOperandTextInputLayout.editText?.text.toString()
-            answerTextView.text = getString(R.string.answer, op1, op2)
+        plusButton.setOnClickListener{
+            var op1: String = firstOperandTextInputLayout.editText?.text.toString()
+            var op2: String = secondOperandTextInputLayout.editText?.text.toString()
 
+            // if op1 is "", set to "0"
+            var intOp1 = 0
+            var intOp2 = 0
+            if(op1 != ""){
+                 intOp1 = op1.toInt()
+            } else {
+                op1 = "0"
+                intOp1 = op1.toInt()
+            }
+            if(op2 != ""){
+                intOp2 = op2.toInt()
+            } else {
+                op2 = "0"
+                intOp2 = op2.toInt()
+            }
+
+            val sum = intOp1+intOp2
+            answerTextView.text = sum.toString()
+        }
+
+        minusButton.setOnClickListener{
+            var op1: String = firstOperandTextInputLayout.editText?.text.toString()
+            var op2: String = secondOperandTextInputLayout.editText?.text.toString()
+
+            // if op1 is "", set to "0"
+            var intOp1 = 0
+            var intOp2 = 0
+            if(op1 != ""){
+                intOp1 = op1.toInt()
+            } else {
+                op1 = "0"
+                intOp1 = op1.toInt()
+            }
+            if(op2 != ""){
+                intOp2 = op2.toInt()
+            } else {
+                op2 = "0"
+                intOp2 = op2.toInt()
+            }
+
+            val diff = intOp1-intOp2
+            answerTextView.text = diff.toString()
+        }
+
+        timesButton.setOnClickListener{
+            var op1: String = firstOperandTextInputLayout.editText?.text.toString()
+            var op2: String = secondOperandTextInputLayout.editText?.text.toString()
+
+            // if op1 is "", set to "0"
+            var intOp1 = 0
+            var intOp2 = 0
+            if(op1 != ""){
+                intOp1 = op1.toInt()
+            } else {
+                op1 = "0"
+                intOp1 = op1.toInt()
+            }
+            if(op2 != ""){
+                intOp2 = op2.toInt()
+            } else {
+                op2 = "0"
+                intOp2 = op2.toInt()
+            }
+
+            val prod = intOp1*intOp2
+            answerTextView.text = prod.toString()
+        }
+
+        divideButton.setOnClickListener{
+            var op1: String = firstOperandTextInputLayout.editText?.text.toString()
+            var op2: String = secondOperandTextInputLayout.editText?.text.toString()
+
+            // if op1 is "", set to "0"
+            var intOp1 = 0
+            var intOp2 = 0
+            if(op1 != ""){
+                intOp1 = op1.toInt()
+            } else {
+                op1 = "0"
+                intOp1 = op1.toInt()
+            }
+            if(op2 != ""){
+                intOp2 = op2.toInt()
+            } else {
+                op2 = "0"
+                intOp2 = op2.toInt()
+            }
+
+            // division by sero
+            if(intOp2==0){
+                answerTextView.text = "Can't divide by 0"
+            } else{
+                val div = intOp1/intOp2
+                answerTextView.text = div.toString()
+            }
         }
     }
-
-//    private fun getAnswer(){
-//
-//    }
 }
